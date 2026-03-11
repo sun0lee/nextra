abdf89884ab20d8ae2c51bd0d937784166bf3bc9
sun0lee <sun0lee88@gmail.com>
2026-03-11 16:30:08 +0900

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,6 @@ export const config = {
      * 1. 아래 단어로 시작하는 모든 경로 제외
      * 2. 아래 확장자로 끝나는 모든 정적 파일 제외 (이미지, 폰트, json 등)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|img|_pagefind|robots|sitemap|ads\\.txt|security\\.txt|\\.well-known|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico|json|txt|xml|pdf|woff2?|css|js)$).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|img|_pagefind|robots|sitemap|ko/sitemap|en/sitemap|ads\\.txt|security\\.txt|\\.well-known|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico|json|txt|xml|pdf|woff2?|css|js)$).*)',
   ],
 }
